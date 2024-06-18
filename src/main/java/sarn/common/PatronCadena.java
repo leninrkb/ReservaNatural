d0df86d32f5f6ad9d79e7570af7c4b089df75a96
leninrkb <rkb.lenin@gmail.com>
2024-06-18 22:33:24 +0000

    corregido regex solo letras

--- src/main/java/sarn/common/PatronCadena.java
+++ src/main/java/sarn/common/PatronCadena.java
@@ -3,6 +3,6 @@ package sarn.common;
 import java.util.regex.Pattern;
 
 public class PatronCadena {
-    public static String regexSinNumeros = "^[^\\\\d]*$";
+    public static String regexSinNumeros = "^[a-zA-Z]*$";
     public static Pattern textoSinNumeros = Pattern.compile(regexSinNumeros);
 }
