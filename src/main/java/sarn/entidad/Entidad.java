4f7c22f6be20235ad8636ad1b816105045d6ae80
JoannD25 <joaorosero1a@hotmail.com>
2024-06-20 03:42:29 +0000

    cambios

--- src/main/java/sarn/entidad/Entidad.java
+++ src/main/java/sarn/entidad/Entidad.java
@@ -1,6 +1,6 @@
 package sarn.entidad;
 import java.io.Serializable;
-import java.util.List;
+//import java.util.List;
 
 public abstract class Entidad implements Serializable {
     protected static final long serialVersionUID = 1L;
